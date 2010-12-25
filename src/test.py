bdf5716abca845fb1cf20bee62cd59a6bf960243
cvs <cvs>
2010-12-25 22:28:21 +0000

    Update to match new syntax and functionaility

--- src/test.py
+++ src/test.py
@@ -1,38 +1,30 @@
+# Test Suite for Stoner package
+# $Log: test.py,v $
+# Revision 1.2  2010/12/25 22:28:21  cvs
+# Update to match new syntax and functionaility
+#
+
 import Stoner
+from scipy import *
 
-# Test Imports
-import numpy
-import pylab
-import tkFileDialog
-import time
-
+# Basic Tests of DataFile functionaluity
 file=Stoner.DataFile()
-file.get_data('data.txt')
+file.load('../sample-data/TDI_Format_RT.txt')
 
-print(file.do_polyfit(1,2,1))
+print file.column_headers
+print file.data
+print file.metadata
+print file.typehint
+print file
 
-'''
-i=0
-while i < 6:
-    time.sleep(1)
-    file.plot_simple_xy(1, 2,'','')
-    i+=1
-    print(i)
-   
-pylab.ioff()
-'''
-# examples:
-def open_directory():
-    filename = tkFileDialog.askdirectory()
-    print filename  # test
-'''  
-def save_it():
-    filename = tkFileDialog.askopenfilename()
-    print filename  # test
-    
-def save_as():
-    filename = tkFileDialog.asksaveasfilename()
-    print filename  # test
-''' 
-#open_directory()
+print len(file)
+for col in file.columns():
+    print "Mean:"+str(mean(col))+" Max:"+str(max(col))+" Min:"+str(min(col))
+ 
+# Tests of PlotFile functionality 
+plot=Stoner.PlotFile(file)
+plot.plot_xy('Temp', 'Res')
+
+# Tests of AnalysisFile Functionality
+
 
