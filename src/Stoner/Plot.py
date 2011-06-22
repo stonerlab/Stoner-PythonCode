e66817c3f3069e5497340eae92f692ec3d10806f
cvs <cvs>
2011-06-22 22:54:29 +0000

    Look at getting PlotFile.plot_xy to plot to the same window and update API documentation

--- src/Stoner/Plot.py
+++ src/Stoner/Plot.py
@@ -2,9 +2,12 @@
 #
 #PlotFile object of the Stoner Package
 #
-# $Id: Plot.py,v 1.7 2011/06/14 08:14:38 cvs Exp $
+# $Id: Plot.py,v 1.8 2011/06/22 22:54:29 cvs Exp $
 #
 # $Log: Plot.py,v $
+# Revision 1.8  2011/06/22 22:54:29  cvs
+# Look at getting PlotFile.plot_xy to plot to the same window and update API documentation
+#
 # Revision 1.7  2011/06/14 08:14:38  cvs
 # Ammended platform.system()=Darwin for correct MacOSx implementation. - CSA
 #
@@ -52,21 +55,67 @@ class PlotFile(DataFile):
     __figure=None
     
     def __init__(self, *args, **kargs): #Do the import of pylab here to speed module load
+        """Constructor of \b PlotFile class. Imports pylab and then calls the parent constructor
+                @param args Arguements to pass to \b DataFile.__init__
+                @param kargs Dictionary of keyword arguments to pass to \b DataFile.__init__
+                
+                @return This instance
+        
+        """
         global pylab
         import pylab
         super(PlotFile, self).__init__(*args, **kargs)
-    def plot_xy(self,column_x, column_y, format=None,show_plot=True,  title='', save_filename='', figure=None, **kwords):
-        """plot_xy(x column, y column/s, title,save filename, show plot=True)
+    
+    def __getattr__(self, name):
+        """Attribute accessor
         
-                Makes and X-Y plot of the specified data."""
+                @param name Name of attribute: only "fig" is supported here to return the current figure refernce
+                
+                All other attrbiutes are passed over to the parent class
+                """
+        if name=="fig":
+            return self.__figure
+        else:
+            super(PlotFile, self).__getattr__(name)
+            
+    def __setattr__(self, name, value):"""
+                Sets the specified attribute
+                
+                @param name The name of the attribute to set. Only "fig" is supported in this class - everything else drops through to the parent class
+                @param value The value of the attribute to set.
+    """
+        if name=="fig":
+            self.figure(value)
+        else:
+            super(PlotFile, self).__setattr__(name, value)
+    
+    def plot_xy(self,column_x, column_y, format=None,show_plot=True,  title='', save_filename='', figure=None, **kwords):
+        """Makes a simple X-Y plot of the specified data.
+                
+                @param column_x An integer or string that indexes the relevant column for the x data
+                @param column_y An integer go string that indexes the y-data column
+                @param format Optional string parameter that specifies the format for the plot - see matplotlib documentation for details
+                @param show_plot = True Turns on interactive plot control
+                @param title Optional parameter that specfies the plot title - otherwise the current DataFile filename is used
+                @param save_filename Filename used to save the plot
+                @param figure Optional argument that controls what matplotlib figure to use. Can be an integer, or a matplotlib.figure or False. If False then a new figure is always used, otherwise it will default to using the last figure used by this DataFile object.
+                @param kwords A dictionary of other keyword arguments to pass into the plot function.
+                
+                @return a matplotlib.figure isntance
+                
+
+        
+        """
         column_x=self.find_col(column_x)
         column_y=self.find_col(column_y)
         x=self.column(column_x)
         y=self.column(column_y)
         if isinstance(figure, int):
             figure=pylab.figure(figure)
+        elif isinstance(figure, bool) and not figure:
+            figure=pylab.figure()
         elif isinstance(figure, matplotlib.figure.Figure):
-            pylab.figure(figure.number)
+            figure=pylab.figure(figure.number)
         elif isinstance(self.__figure,  matplotlib.figure.Figure):
             figure=self.__figure
         else:
@@ -74,9 +123,9 @@ class PlotFile(DataFile):
         if show_plot == True:
             pylab.ion()
         if format==None:
-            pylab.plot(x,y, **kwords)
+            pylab.plot(x,y, figure=figure, **kwords)
         else:
-            figure=pylab.plot(x,y, format, **kwords)        
+            figure=pylab.plot(x,y, format, figure=figure, **kwords)        
         pyplot.draw()
         pylab.xlabel(str(self.column_headers[column_x]))
         if isinstance(column_y, list):
@@ -107,7 +156,7 @@ class PlotFile(DataFile):
         else:
             return None
         
-    def figure(figure):
+    def figure(self, figure):
         """Set the figure used by \b Stoner.PlotFile
          @param figure A matplotlib figure or figure number
          @return The current \b Stoner.PlotFile instance"""
