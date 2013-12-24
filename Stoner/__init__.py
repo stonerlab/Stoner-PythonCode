1834e89a0806c06adf36ca11a1e0a4ed3a3289f1
Gavin Burnell <g.burnell@leeds.ac.uk>
2013-12-24 17:08:01 +0000

    Version bump

--- Stoner/__init__.py
+++ Stoner/__init__.py
@@ -37,6 +37,6 @@ from Plot import *
 from FileFormats import *
 from Folders import *
 
-__version_info__ = ('0', '1', '1')
+__version_info__ = ('0', '1', '3')
 __version__ = '.'.join(__version_info__)
 
