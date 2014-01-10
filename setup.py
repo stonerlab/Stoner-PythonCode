575a98d8d9c54bf93a400064bf368ec3845c8ac8
Gavin Burnell <g.burnell@leeds.ac.uk>
2014-01-10 23:46:02 +0000

    Add the setuptools package and some more sample data and scripts

---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup
+import Stoner
+reload(Stoner)
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "Stoner",
+    version = str(Stoner.__version__),
+    author = "Gavin Burnell",
+    author_email = "g.burnell@leeds.ac.uk",
+    description = ("Classes to represent simple scientific data sets and write analysis codes, developed for the University of Leeds Condensed Matter Physics Group"),
+    license = "GPLv3",
+    keywords = "Data-Analysis Physics",
+    url = "http://github.com/~gb119/Stoner-PythonCode",
+    packages=['Stoner'],
+    test_suite="tests",
+    install_requires=["numpy>1.4","scipy>=0.12","matplotlib>1.2"],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
