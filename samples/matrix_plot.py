00687a328936202bcbd0f14740f5db03fd4251bc
Gavin Burnell <g.burnell@leeds.ac.uk>
2016-09-28 22:12:48 +0100

    Revised versiuon of docs for mixins branch

--- samples/matrix_plot.py
+++ samples/matrix_plot.py
@@ -7,6 +7,7 @@ z=np.cos(4*np.pi*np.sqrt(x**2+y**2))*np.exp(-np.sqrt(x**2+y**2))
 p=Data()
 p=p&np.linspace(-2,2,101)&z
 p.column_headers=["X"]
-p.column_headers.extend([str(i) for i in np.linspace(-2,2,101)])
+for i,v in enumerate(np.linspace(-2,2,101)):
+    p.column_headers[i+1]=str(v)
 
 p.plot_matrix(xlabel="x",ylabel="y",title="Data as Matrix")
