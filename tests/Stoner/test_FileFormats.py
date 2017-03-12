d4a01b4a8cb6c463c3f4400518281b2dce43ceb5
Gavin Burnell <g.burnell@leeds.ac.uk>
2017-03-12 22:04:01 +0000

    Zipfile not a subclass?

--- tests/Stoner/test_FileFormats.py
+++ tests/Stoner/test_FileFormats.py
@@ -40,6 +40,8 @@ class FileFormats_test(unittest.TestCase):
             #return None # skip this completely at this time
         else:
             skip_files=[]
+        d=Data()
+        print(d.subclasses)
         for i,f in enumerate(os.listdir(self.datadir)):
             if f.strip().lower() in skip_files: # Known bad files to load
                 print("Skipping {}".format(f))
