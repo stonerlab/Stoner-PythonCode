319f2ef65196defab2e97c6decc4b309af883e59
Gavin Burnell <g.burnell@leeds.ac.uk>
2017-03-27 23:04:42 +0100

    Rinse and repeat

--- doc/samples/outlier.py
+++ doc/samples/outlier.py
@@ -2,7 +2,7 @@ from Stoner import Data
 import numpy as np
 
 x=np.linspace(0,100,201)
-y=0.01*x**2+5*np.ssin(x/10.0)
+y=0.01*x**2+5*np.sin(x/10.0)
 
 i=np.random.randint(len(x),size=10)
 y[i]+=np.srandom.normal(size=len(i),scale=20)
