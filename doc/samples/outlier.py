01a06127c34c5be37de3e33f0104c38b763e9bab
Gavin Burnell <g.burnell@leeds.ac.uk>
2017-03-27 23:13:29 +0100

    Another stupid typo

--- doc/samples/outlier.py
+++ doc/samples/outlier.py
@@ -5,7 +5,7 @@ x=np.linspace(0,100,201)
 y=0.01*x**2+5*np.sin(x/10.0)
 
 i=np.random.randint(len(x),size=10)
-y[i]+=np.srandom.normal(size=len(i),scale=20)
+y[i]+=np.random.normal(size=len(i),scale=20)
 
 d=Data(np.column_stack((x,y)),column_headers=["x","y"],setas="xy")
 d.plot(fmt="b.",label="raw data")
