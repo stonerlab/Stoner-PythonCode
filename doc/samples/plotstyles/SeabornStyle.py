4afbdf66664c442b86548e95d5643471be74bca7
Gavin Burnell <g.burnell@leeds.ac.uk>
2017-04-24 14:45:17 +0100

    Fix scripts to find their data files in docbuild

--- doc/samples/plotstyles/SeabornStyle.py
+++ doc/samples/plotstyles/SeabornStyle.py
@@ -1,8 +1,8 @@
 """Example plot style using Seaborn plot styling template."""
-from Stoner import Data
+from Stoner import Data,__home__
 from Stoner.plot.formats import SeabornPlotStyle
 import os.path as path
-filename=path.realpath(path.join(path.dirname(__file__),"..","sample.txt"))
+filename=path.realpath(path.join(__home__,"..","doc","samples","sample.txt"))
 d=Data(filename,setas="xyy",template=SeabornPlotStyle(stylename="dark",context="talk",palette="muted"))
 d.plot(multiple="y2")
 
--- doc/samples/plotstyles/default.py
+++ doc/samples/plotstyles/default.py
@@ -1,8 +1,8 @@
 """Example plot using default style."""
-from Stoner import Data
+from Stoner import Data,__home__
 from Stoner.plot.formats import DefaultPlotStyle
 import os.path as path
-filename=path.realpath(path.join(path.dirname(__file__),"..","sample.txt"))
+filename=path.realpath(path.join(__home__,"..","doc","samples","sample.txt"))
 d=Data(filename,setas="xy",template=DefaultPlotStyle)
 d.plot()
 
