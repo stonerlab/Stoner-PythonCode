31d9443fb5e59cb68a20657eafe0880cb53c0b97
Gavin Burnell <g.burnell@leeds.ac.uk>
2017-04-24 15:05:52 +0100

    Update documentation with working plot.format templates

--- samples/plotstyles/PRBStyle.py
+++ samples/plotstyles/PRBStyle.py
@@ -1,8 +1,8 @@
 """Example plot using a style similar to Physical Review B."""
-from Stoner import Data
+from Stoner import Data,__home__
 from Stoner.plot.formats import PRBPlotStyle
 import os.path as path
-filename=path.realpath(path.join(path.dirname(__file__),"..","sample.txt"))
+filename=path.realpath(path.join(__home__,"..","doc","samples","sample.txt"))
 d=Data(filename,setas="xy",template=PRBPlotStyle)
 d.plot()
 
