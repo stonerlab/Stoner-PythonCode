3340e91d7eb7a7f6bd0637a765d1cb09642fc7bc
Gavin Burnell <g.burnell@leeds.ac.uk>
2017-04-29 20:12:31 +0100

    Back off 3D plot as make test is having trouble closing the figure

--- doc/samples/Vectorfield.py
+++ doc/samples/Vectorfield.py
@@ -3,25 +3,18 @@ from Stoner import Data,__home__
 from os import path
 d=Data(path.join(__home__,"..","sample-data","OVF1.ovf"))
 e=d.select(Z__between=(10,11)).select(X__between=(10,18)).select(Y__between=(5,13))
-e.figure(figsize=(12,4))
+e.figure(figsize=(8,4))
 
 #2D vectors on a 2D Field
 e.setas="xy.uv."
-e.subplot(131)
+e.subplot(121)
 e.plot()
 e.title="2D Vector, 2D Field"
 
-fig=e.fig
-
-#3D Vectors ona 3D Field
-e.fig.add_subplot(133,projection="3d")
-e.setas="xyzuvw"
-e.plot(length=0.4,linewidth=1.5,arrow_length_ratio=.5,pivot="middle")
-e.title="3D Vector, 3D Field"
-e.tight_layout()
-
 #3D Vector on a 2D Field
-e.subplot(132)
+e.subplot(122)
 e.setas="xy.uvw"
 e.plot()
 e.title="3D Vector, 2D Field"
+
+e.tight_layout()
