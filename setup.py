f60f469f704aae3cfaa465e88cfe0e17eae73eea
Gavin Burnell <g.burnell@leeds.ac.uk>
2017-05-15 09:45:52 +0100

    Build wheel

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     package_data={'Stoner':['stylelib/*.mplstyle']},
     test_suite="tests",
     install_requires=["numpy>=1.7","scipy>=0.14","matplotlib>=1.5","h5py","lmfit","filemagic","pillow","scikit-image"],
-    long_description=read('README.rst'),
+    long_description=read('doc/readme.rst'),
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
