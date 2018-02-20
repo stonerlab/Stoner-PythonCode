af87353f5932ae7974328bac6d1621c821afeac6
Gavin Burnell <g.burnell@leeds.ac.uk>
2018-02-20 20:51:23 +0000

    Move ImageFolder functionality to a mixin class ImageFolderMixin and then make ImageFolder inherit from this and DiskBasedFolder and baseFolder.

--- Stoner/Image/folders.py
+++ Stoner/Image/folders.py
@@ -20,19 +20,19 @@ def _load_ImageArray(f, **kargs):
     return ImageArray(f, **kargs)
 
 class _generator(object):
-    
+
     """A helper class to iterator over ImageFolder yet remember it's own length."""
-    
+
     def __init__(self,fldr):
         self.fldr=fldr
         self.len=len(fldr)
-        
+
     def __len__(self):
         return self.len
-    
+
     def __iter__(self):
         return self
-    
+
     def __next__(self):
         for i in self.fldr:
             if hasattr(i,"image"):
@@ -50,13 +50,13 @@ class _generator(object):
     next=__next__
 
 
-class ImageFolder(DiskBssedFolder,baseFolder):
-    
-    """Folder object for images.
-    
-    ImageFolder is designed to behave pretty much like DataFolder but with
+class ImageFolderMixin(object):
+
+    """Mixin to provide a folder object for images.
+
+    ImageFolderMixin is designed to behave pretty much like DataFolder but with
     functions and loaders appropriate for image based files.
-    
+
         Attributes:
         type (:py:class:`Stoner.Image.core.ImageArray`) the type ob object to sotre in the folder (defaults to :py:class:`Stoner.Cire.Data`)
 
@@ -76,31 +76,31 @@ class ImageFolder(DiskBssedFolder,baseFolder):
         directory (str): The root directory on disc for the folder - by default this is the current working directory.
 
         multifile (boo): Whether to select individual files manually that are not (necessarily) in  a common directory structure.
-        
+
         readlist (bool): Whether to read the directory immediately on creation. Default is True
     """
 
     def __init__(self, *args, **kargs):
-        """nitialise the ImageFolder. 
-        
+        """nitialise the ImageFolder.
+
         Mostly a pass through to the :py:class:`Stoner.Folders.baseFolder` class.
         """
         kargs["pattern"]=kargs.get("pattern","*.png")
         kargs["type"]=kargs.get("type",ImageArray)
         if "flat" in self._defaults:
             del self._defaults["flat"]
-        super(ImageFolder,self).__init__(*args,**kargs)
+        super(ImageFolderMixin,self).__init__(*args,**kargs)
 
     @property
     def images(self):
         """A generator that iterates over just the images in the Folder."""
-        return _generator(self)        
-    
+        return _generator(self)
+
     def loadgroup(self):
         """Load all files from this group into memory"""
         for _ in self:
             pass
-        
+
     def slice_metadata(self, key=None, values_only=False):  # pylint: disable=arguments-differ
         """Return a list of the metadata dictionaries for each item/file in the top level group
 
@@ -114,8 +114,8 @@ class ImageFolder(DiskBssedFolder,baseFolder):
             ret(list of dict, tuple or values):
                 depending on values_only returns the sliced dictionaries or tuples/
                 values of the items
-            
-        To do: 
+
+        To do:
             this should probably be a func in baseFolder and should use have
             recursive options (build a dictionary of metadata values). And probably
             options to extract other parts of objects (first row or whatever).
@@ -138,13 +138,13 @@ class ImageFolder(DiskBssedFolder,baseFolder):
             if len(metadata[0])==1: #single key
                 metadata=[m[0] for m in metadata]
         return metadata
-    
+
     def as_stack(self):
         """Return a ImageStack of the images in the current group."""
         from Stoner.Image import ImageStack
         k = ImageStack(self)
         return k
-    
+
     def mean(self):
         """Calculate the mean value of all the images in the stack."""
         total=np.zeros_like(self[0])
@@ -160,15 +160,44 @@ class ImageFolder(DiskBssedFolder,baseFolder):
             ret.metadata.update(self[0].metadata)
         else:
             ret=total
-        return ret            
-        
-    
+        return ret
+
+
     def view(self):
         """Create a matplotlib animated view of the contents.
-                      
+
         """
         cv=CollectionViewer(self.images)
         cv.show()
         return cv
-        
 
+class ImageFolder(ImageFolderMixin,DiskBssedFolder,baseFolder):
+
+    """Folder object for images.
+
+    ImageFolder is designed to behave pretty much like DataFolder but with
+    functions and loaders appropriate for image based files.
+
+        Attributes:
+        type (:py:class:`Stoner.Image.core.ImageArray`) the type ob object to sotre in the folder (defaults to :py:class:`Stoner.Cire.Data`)
+
+        extra_args (dict): Extra arguments to use when instantiatoing the contents of the folder from a file on disk.
+
+        pattern (str or regexp): A filename globbing pattern that matches the contents of the folder. If a regular expression is provided then
+            any named groups are used to construct additional metadata entryies from the filename. Default is *.* to match all files with an extension.
+
+        read_means (bool): IF true, additional metatdata keys are added that return the mean value of each column of the data. This can hep in
+            grouping files where one column of data contains a constant value for the experimental state. Default is False
+
+        recursive (bool): Specifies whether to search recurisvely in a whole directory tree. Default is True.
+
+        flatten (bool): Specify where to present subdirectories as spearate groups in the folder (False) or as a single group (True). Default is False.
+            The :py:meth:`DiskBasedFolder.flatten` method has the equivalent effect and :py:meth:`DiskBasedFolder.unflatten` reverses it.
+
+        directory (str): The root directory on disc for the folder - by default this is the current working directory.
+
+        multifile (boo): Whether to select individual files manually that are not (necessarily) in  a common directory structure.
+
+        readlist (bool): Whether to read the directory immediately on creation. Default is True
+    """
+    pass
