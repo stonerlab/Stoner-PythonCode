f895219b836591f6e9d319dc876d30cfa7b07151
Gavin Burnell <g.burnell@leeds.ac.uk>
2018-06-12 10:26:14 +0100

    Rebuilt docs

--- samples/Fitting/vft-class.py
+++ samples/Fitting/vft-class.py
@@ -1,6 +1,6 @@
 """Example of Arrhenius Fit."""
 from Stoner import Data
-import Stoner.Fit as SF
+from Stoner.Fit import vftEquation,VFTEquation
 from  numpy import logspace,log10
 from numpy.random import normal
 
@@ -8,27 +8,43 @@ from numpy.random import normal
 T=logspace(log10(200),log10(350),51)
 params=(1E16,0.5,150)
 noise=0.5
-R=SF.vftEquation(T,*params)*normal(size=len(T),scale=noise,loc=1.0)
-dR=SF.vftEquation(T,*params)*noise
+R=vftEquation(T,*params)*normal(size=len(T),scale=noise,loc=1.0)
+dR=vftEquation(T,*params)*noise
+d=Data(T,R,dR,setas="xy.",column_headers=["T","Rate"])
 
-d=Data(T,R,dR,setas="xye",column_headers=["T","Rate"])
+#Plot the data points.
+d.plot(fmt="k+",label="Data Points")
+
+#Turn on the sigma column (error bars look messy on plot due to logscale)
+d.setas[2]="e"
 
 #Curve_fit on its own
-d.curve_fit(SF.vftEquation,p0=params,result=True,header="curve_fit")
+d.curve_fit(vftEquation,p0=params,result=True,header="curve_fit")
 
-# lmfit using lmfit guesses
-fit=SF.VFTEquation()
+# lmfit uses some guesses
 p0=params
-d.lmfit(fit,p0=p0,result=True,header="lmfit")
+d.lmfit(VFTEquation,p0=p0,result=True,header="lmfit")
 
-d.setas="xyeyyy"
-d.plot(fmt=["k+","r-","b-"])
+#Plot these results too
+d.setas="x..yy"
+d.plot(fmt=["r-","b-"])
+#Annotate the graph
+d.annotate_fit(vftEquation,x=290,y=1E-11,fontdict={"size":"x-small"},mode="eng")
+d.annotate_fit(VFTEquation,x=290,y=1E-22,prefix="VFTEquation",fontdict={"size":"x-small"},mode="eng")
+
+#reset the columns for the fit
+d.setas="xye.."
+#Now do the odr fit (will overwrite lmfit's metadata)
+d.odr(VFTEquation,p0=p0)
+d.setas="x4.y"
+
+# And plot and annotate
+d.plot(fmt="g-",label="Orthogonal distance")
+d.annotate_fit(VFTEquation,x=290,y=1E-33,fontdict={"size":"x-small"},mode="eng")
+
+# Finally tidy up the plot a bit
 d.yscale="log"
 d.ylim=(1E-35,1E10)
-d.annotate_fit(SF.vftEquation,x=290,y=100,fontdict={"size":"x-small"},mode="eng")
-
-d.annotate_fit(SF.VFTEquation,x=240,y=100,prefix="VFTEquation",fontdict={"size":"x-small"},mode="eng")
-
 d.title="VFT Equation Test Fit"
 d.ylabel="Rate"
 d.xlabel="Temperature (K)"
