21ee66428fa9acc40966152a3bd440636b49fca4
Gavin Burnell <g.burnell@leeds.ac.uk>
2018-06-23 23:21:45 +0100

    Start rearranging Core into a sub-package of smaller files

---
+++ Stoner/core/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 23 18:50:37 2018
+
+@author: phygbu
+"""
+__all__=["metadataObject","typeHintedDict","regexpDict","_setas"]
+from .base import regexpDict,typeHintedDict,metadataObject
+from .setas import _setas
---
+++ Stoner/core/exceptions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Stoner Package specific exceptions
+"""
+
+class StonerLoadError(Exception):
+
+    """An exception thrown by the file loading routines in the Stoner Package.
+
+    This special exception is thrown when one of the subclasses of :py:class:`Stoner.Core.DataFile`
+    attmpts and then fails to load some data from disk. Generally speaking this is not a real
+    error, but simply indicates that the file format is not recognised by that particular subclass,
+    and thus another subclass should have a go instead.
+    """
+
+    pass
+
+class StonerSetasError(AttributeError):
+
+    """An exception tjrown when we try to access a column in data without setas being set."""
+
+    pass
+
