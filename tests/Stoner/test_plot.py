1f7b1b22d7fc1476514be0ee15bc9e7ccb00f96c
Gavin Burnell <g.burnell@leeds.ac.uk>
2018-06-27 23:52:55 +0100

    Add test of no_figs option and Data.showfig

---
+++ tests/Stoner/test_plot.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+test_Core.py
+Created on Tue Jan 07 22:05:55 2014
+
+@author: phygbu
+"""
+
+import unittest
+import sys
+import os, os.path as path
+import numpy as np
+import re
+from numpy import any,all,sqrt,nan
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+
+pth=path.dirname(__file__)
+pth=path.realpath(path.join(pth,"../../"))
+sys.path.insert(0,pth)
+from Stoner import Data,__home__,set_option,get_option
+from Stoner.Core import typeHintedDict
+
+class Plottest(unittest.TestCase):
+
+    """Path to sample Data File"""
+    datadir=path.join(pth,"sample-data")
+
+    def setUp(self):
+        self.d=Data(path.join(__home__,"..","sample-data","New-XRay-Data.dql"))
+
+    def test_set_no_figs(self):
+        self.assertTrue(get_option("no_figs"),"Default setting for no_figs option is incorrect.")
+        set_option("no_figs",False)
+        e=self.d.clone
+        ret=e.plot()
+        self.assertTrue(ret is None,"Output of Data.plot() was not None when no_figs is True ({})".format(type(ret)))
+        e.showfig=False
+        ret=e.plot()
+        self.assertTrue(isinstance(ret,Data),"Return value of Data.plot() was not self when Data.showfig=False ({})".format(type(ret)))
+        e.showfig=True
+        ret=e.plot()
+        self.assertTrue(isinstance(ret,Figure),"Return value of Data.plot() was not Figure when Data.showfig=False({})".format(type(ret)))
+        e.showfig=None
+        ret=e.plot()
+        self.assertTrue(ret is None,"Output of Data.plot() was not None when Data.showfig is None ({})".format(type(ret)))
+        set_option("no_figs",True)
+        self.assertTrue(get_option("no_figs"),"set_option no_figs failed.")
+        self.d=Data(path.join(__home__,"..","sample-data","New-XRay-Data.dql"))
+        self.d.showfig=False
+        ret=self.d.plot()
+        self.assertTrue(ret is None,"Output of Data.plot() was not None when no_figs is True ({})".format(type(ret)))
+        set_option("no_figs",True)
+        plt.close("all")
+
+
+
+
+
+if __name__=="__main__": # Run some tests manually to allow debugging
+    test=Plottest("test_set_no_figs")
+    test.setUp()
+    unittest.main()
+    #set_option("no_figs",True)
