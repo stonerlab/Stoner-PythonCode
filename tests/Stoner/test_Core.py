a311f350fc5fb0e685afdf8e29c8eea66e76cc8a
Gavin Burnell <g.burnell@leeds.ac.uk>
2018-12-10 16:42:18 +0000

    Remove duplicate test

--- tests/Stoner/test_Core.py
+++ tests/Stoner/test_Core.py
@@ -380,54 +380,6 @@ Stoner.class{String}= Data          0  ...             0  ...             0     
         os.remove(path.join(local, "mixedmetatest.txt")) #clear up
         os.remove(path.join(local, "mixedmetatest2.txt"))
 
-    def test_setas_metadata(self):
-        d2=self.d2.clone
-        d2.setas+={"x":0,"y":1}
-        self.assertEqual(d2.setas.to_dict(),{'x': 'Temperature', 'y': 'Resistance'},"__iadd__ failure {}".format(repr(d2.setas.to_dict())))
-        d3=self.d2.clone
-        d3.setas="..e"
-        d2.setas.update(d3.setas)
-        self.assertEqual(d2.setas.to_dict(),{'x': 'Temperature', 'y': 'Resistance', 'e': 'Column 2'},"__iadd__ failure {}".format(repr(d2.setas.to_dict())))
-        auto_setas={ 'axes': 2,
-                     'has_axes': True,
-                     'has_ucol': False,
-                     'has_uvw': False,
-                     'has_vcol': False,
-                     'has_wcol': False,
-                     'has_xcol': True,
-                     'has_xerr': False,
-                     'has_ycol': True,
-                     'has_yerr': True,
-                     'has_zcol': False,
-                     'has_zerr': False,
-                     'ucol': [],
-                     'vcol': [],
-                     'wcol': [],
-                     'xcol': 0,
-                     'xerr': None,
-                     'ycol': [1],
-                     'yerr': [2],
-                     'zcol': [],
-                     'zerr': []}
-        self.assertEqual(self.d2.setas._get_cols(),auto_setas,"Automatic guessing of setas failed!")
-        d2.setas.clear()
-        self.assertEqual(list(d2.setas),["."]*3,"Failed to clear() setas")
-        d2.setas[[0,1,2]]="x","y","z"
-        self.assertEqual("".join(d2.setas),"xyz","Failed to set setas with a list")
-        d2.setas[[0,1,2]]="x"
-        self.assertEqual("".join(d2.setas),"xxx","Failed to set setas with an element from a list")
-        d=self.d.clone
-        d.setas="xyz"
-        self.assertEqual(repr(d.setas),"['x', 'y']","setas __repr__ failure {}".format(repr(d.setas)))
-        self.assertEqual(d.find_col(slice(5)),[0,1],"findcol with a slice failed {}".format(d.find_col(slice(5))))
-        d=self.d2.clone
-        d.setas="xyz"
-        self.assertTrue(d["Column 2",5]==d[5,"Column 2"],"Indexing with mixed integer and string failed.")
-        self.assertEqual(d.metadata.type(["User","Timestamp"]),['String', 'String'],"Metadata.type with slice failed")
-        d.data["Column 2",:]=np.zeros(len(d)) #TODO make this work with d["Column 2",:] as well
-        self.assertTrue(d.z.max()==0.0 and d.z.min()==0.0,"Failed to set Dataarray using string indexing")
-
-
 if __name__=="__main__": # Run some tests manually to allow debugging
     test=Datatest("test_operators")
     test.setUp()
