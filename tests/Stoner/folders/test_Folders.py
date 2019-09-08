f769674f857a27629912e276d2bc977d65818957
Gavin Burnell <g.burnell@leeds.ac.uk>
2019-09-08 09:41:02 +0100

    Fix indetation error

--- tests/Stoner/folders/test_Folders.py
+++ tests/Stoner/folders/test_Folders.py
@@ -266,6 +266,6 @@ class Folders_test(unittest.TestCase):
 if __name__=="__main__": # Run some tests manually to allow debugging
     test=Folders_test("test_Folders")
     test.setUp()
-   unittest.main()
+    unittest.main()
     #test.test_Properties()
 
