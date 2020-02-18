Fixing up isses with Image code that were breaking tests. Fix bug with image scaling when downcastingh integer images

Stoner/Image/core.py
@@ -600,9 +600,9 @@ class ImageArray(np.ma.MaskedArray, metadataObject):
             """Wrapped magic proxy function call."""
             transpose = getattr(workingfunc, "transpose", False)
             if transpose:
-                change = np.copy(self).T.view(type=self.__class__)
+                change = self.clone.T
             else:
-                change = np.copy(self).view(type=self.__class__)
+                change = self.clone
             r = workingfunc(change, *args, **kwargs)  # send copy of self as the first arg
             if isinstance(r, make_Data(None)):
                 pass  # Data return is ok
@@ -1353,7 +1353,7 @@ class ImageFile(metadataObject):
                     # Now swap the iamge in, but keep the metadata
                 metadata = ret.metadata
                 filename = ret.filename
-                ret.image = np.atleast_2d(r).view(ImageArray)
+                ret.image = np.atleast_2d(r).view(self.image.__class__)
                 ret.filename = filename
                 ret.metadata = metadata
                 return ret

Stoner/Image/imagefuncs.py
@@ -375,11 +375,11 @@ def convert(image, dtype, force_copy=False, uniform=False, normalise=True):
 
     if dtype_in == dtype:
         if force_copy:
-            image = image.copy()
+            image = image.clone
         return image
 
     if not (dtype_in in _supported_types and dtype in _supported_types):
-        raise ValueError("can not convert %s to %s." % (dtypeobj_in, dtypeobj))
+        raise ValueError(f"can not convert {dtype_in} to {dtype}.")
 
     kind = dtypeobj.kind
     kind_in = dtypeobj_in.kind
@@ -395,9 +395,7 @@ def convert(image, dtype, force_copy=False, uniform=False, normalise=True):
 
     if kind_in == "b":
         # from binary image, to float and to integer
-        result = image.astype(dtype)
-        if kind != "f":
-            result *= dtype(dtype_range[dtype][1])
+        result = np.where(~image, *dtype_range[dtype])
         return result
 
     if kind in "ui":

Stoner/Image/util.py
@@ -80,7 +80,7 @@ def _scale(a, n, m, dtypeobj_in, dtypeobj, copy=True):
     Numbers can be represented exactly only if m is a multiple of n
     Output array is of same kind as input."""
     kind = a.dtype.kind
-    if n > m and a.max() < 2 ** m:
+    if n > m and a.max() > 2 ** m:
         mnew = int(np.ceil(m / 2) * 2)
         if mnew > m:
             dtype = "int%s" % mnew

tests/Stoner/Image/test_core.py
@@ -81,7 +81,7 @@ class ImageArrayTest(unittest.TestCase):
             ims[fmt].save_tiff(path.join(tmpdir,"kermit-forcetype{}.tiff".format(fmt)),forcetype=True)
             ims[fmt].save_npy(path.join(tmpdir,"kermit-{}.npy".format(fmt)))
             if fmt!="uint16":
-                im=Image.fromarray((ims[fmt].view(np.ndarray)),modes[fmt])
+                im=Image.fromarray((ims[fmt].image.view(np.ndarray)),mode=modes[fmt])
                 im.save(path.join(tmpdir,"kermit-nometadata-{}.tiff".format(fmt)))
             del ims[fmt]["Loaded from"]
         for fmt in fmts:

tests/Stoner/Image/test_stack.py
@@ -71,7 +71,7 @@ class ImageStackTest(unittest.TestCase):
         self.im1=self.istack2[0]
         self.im1.normalise()
         self.im1.convert(np.int32)
-        self.im2=self.im1.convert(np.float32)
+        self.im2=self.im1.convert(np.float32, force_copy=True)
         conv_err=(self.istack2[0].image-self.im2.image).max()
         self.assertTrue(conv_err<1E-7,"Problems double converting images:{}.".format(conv_err))
         self.im1=self.istack2[0].convert(np.int64)
@@ -168,7 +168,7 @@ if __name__=="__main__":
     test=ImageStackTest()
     test.setUp()
     #test.test_methods()
-    test.test_ImageStack()
+    #test.test_ImageStack()
     #test.test_mask()
-    #unittest.main()
+    unittest.main()
 