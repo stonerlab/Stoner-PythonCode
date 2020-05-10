493d93faddbcb3f71535559f636e92ce3c3b4b6c
Gavin Burnell <g.burnell@leeds.ac.uk>
2020-05-10 19:50:53 +0100

    Allow PyQT5 to be an optional dependency (good for RTD building)

--- Stoner/tools/widgets.py
+++ Stoner/tools/widgets.py
@@ -6,112 +6,133 @@ Code based on the PyQt5 Tutorial code,
 __all__ = ["fileDialog"]
 import pathlib
 from typing import Any, Union, Optional, Dict, Type
-from PyQt5.QtWidgets import QWidget, QFileDialog, QApplication
+
+try:
+    from PyQt5.QtWidgets import QWidget, QFileDialog, QApplication
+except ImportError:
+
+    class App:
+
+        """Mock App that raises an error when you try to call openDialog on it."""
+
+        modes: Dict = dict()
+
+        def openDialog(
+            self,
+            title: Optional[str] = None,
+            start: Union[str, pathlib.Path] = "",
+            patterns: Optional[Dict] = None,
+            mode: str = "OpenFile",
+        ) -> Optional[pathlib.Path]:
+            """Raise and error because PyQT5 not present."""
+            raise ValueError("Cammpt open a dialog box because PqQt5 library missing!")
 
 
-class App(QApplication):
+else:
 
-    """Placehold PyQT5 Application for producing filedialog boxes."""
+    class App(QApplication):
 
-    modes = {
-        "OpenFile": {
-            "method": QFileDialog.getOpenFileName,
-            "caption": "Select file to open...",
-            "arg": ["parent", "caption", "directory", "filter", "options"],
-        },
-        "OpenFiles": {
-            "method": QFileDialog.getOpenFileNames,
-            "caption": "Select file(s_ to open...",
-            "arg": ["parent", "caption", "directory", "filter", "options"],
-        },
-        "SaveFile": {
-            "method": QFileDialog.getSaveFileName,
-            "caption": "Save file as...",
-            "arg": ["parent", "caption", "directory", "filter", "options"],
-        },
-        "SelectDirectory": {
-            "method": QFileDialog.getExistingDirectory,
-            "caption": "Select folder...",
-            "arg": ["parent", "caption", "directory", "options"],
-        },
-    }
+        """Placehold PyQT5 Application for producing filedialog boxes."""
 
-    def __init__(self, *args: Any, **kargs: Any) -> None:
-        super().__init__([""], *args, **kargs)
-        self.title = "PyQt5 file dialogs - pythonspot.com"
-        self.left = 10
-        self.top = 10
-        self.width = 640
-        self.height = 480
-        self.initUI()
+        modes = {
+            "OpenFile": {
+                "method": QFileDialog.getOpenFileName,
+                "caption": "Select file to open...",
+                "arg": ["parent", "caption", "directory", "filter", "options"],
+            },
+            "OpenFiles": {
+                "method": QFileDialog.getOpenFileNames,
+                "caption": "Select file(s_ to open...",
+                "arg": ["parent", "caption", "directory", "filter", "options"],
+            },
+            "SaveFile": {
+                "method": QFileDialog.getSaveFileName,
+                "caption": "Save file as...",
+                "arg": ["parent", "caption", "directory", "filter", "options"],
+            },
+            "SelectDirectory": {
+                "method": QFileDialog.getExistingDirectory,
+                "caption": "Select folder...",
+                "arg": ["parent", "caption", "directory", "options"],
+            },
+        }
 
-    def initUI(self) -> None:
-        self.dialog = QWidget()
-        self.dialog.title = "PyQt5 file dialogs - pythonspot.com"
-        self.dialog.left = 10
-        self.dialog.top = 10
-        self.dialog.width = 640
-        self.dialog.height = 480
+        def __init__(self, *args: Any, **kargs: Any) -> None:
+            super().__init__([""], *args, **kargs)
+            self.title = "PyQt5 file dialogs - pythonspot.com"
+            self.left = 10
+            self.top = 10
+            self.width = 640
+            self.height = 480
+            self.initUI()
 
-        self.dialog.setWindowTitle(self.title)
-        self.dialog.setGeometry(self.left, self.top, self.width, self.height)
+        def initUI(self) -> None:
+            self.dialog = QWidget()
+            self.dialog.title = "PyQt5 file dialogs - pythonspot.com"
+            self.dialog.left = 10
+            self.dialog.top = 10
+            self.dialog.width = 640
+            self.dialog.height = 480
 
-    def openDialog(
-        self,
-        title: Optional[str] = None,
-        start: Union[str, pathlib.Path] = "",
-        patterns: Optional[Dict] = None,
-        mode: str = "OpenFile",
-    ) -> Optional[pathlib.Path]:
-        """Create a dialog box for selecting filenames or directories.
+            self.dialog.setWindowTitle(self.title)
+            self.dialog.setGeometry(self.left, self.top, self.width, self.height)
 
-        Keyword Arguments:
-            title (str, None):
-                Label of the dialog box, default None will select something depending on the mode.
-            start (str):
-                The starting directory for the dialog box.
-            patterns (dict):
-                Filename patterns - the keys of the dictionary are glob patterns, the values the corresponding
-                explanation of the file
-                type.
-            mode (str):
-                Determines the type of filedialog box used. Values are:
-                    -   "OpenFile" - get a single existing file
-                    -   "OpenFiles" - get one or more existing files
-                    -   "SaveFile" - get an existing or new file - warns about overwriting files
-                    -   "SelectDirectory" - gets the name of an existing (possibly newly created) directory.
+        def openDialog(
+            self,
+            title: Optional[str] = None,
+            start: Union[str, pathlib.Path] = "",
+            patterns: Optional[Dict] = None,
+            mode: str = "OpenFile",
+        ) -> Optional[pathlib.Path]:
+            """Create a dialog box for selecting filenames or directories.
 
-        Returns:
-            (str, None):
-                Either a string containing the absolute path to the file or directory, or None if the dialog
-                was cancelled.
-        """
-        if mode not in self.modes:
-            raise ValueError(f"Unknown dialog mode {mode}")
-        method = self.modes[mode]["method"]
-        if title is None:
-            title = self.modes[mode]["caption"]
-        if patterns is None:
-            patterns = {"*.*": "All Files", "*.py": "Python Files"}
-        patterns = ";;".join([f"{v} ({k})" for k, v in patterns.items()])
-        options = QFileDialog.Options()
+            Keyword Arguments:
+                title (str, None):
+                    Label of the dialog box, default None will select something depending on the mode.
+                start (str):
+                    The starting directory for the dialog box.
+                patterns (dict):
+                    Filename patterns - the keys of the dictionary are glob patterns, the values the corresponding
+                    explanation of the file
+                    type.
+                mode (str):
+                    Determines the type of filedialog box used. Values are:
+                        -   "OpenFile" - get a single existing file
+                        -   "OpenFiles" - get one or more existing files
+                        -   "SaveFile" - get an existing or new file - warns about overwriting files
+                        -   "SelectDirectory" - gets the name of an existing (possibly newly created) directory.
 
-        kwargs = {"caption": title, "directory": start, "filter": patterns, "options": options}
-        kwargs = {k: kwargs[k] for k in (set(kwargs.keys()) & set(self.modes[mode]["arg"]))}
+            Returns:
+                (str, None):
+                    Either a string containing the absolute path to the file or directory, or None if the dialog
+                    was cancelled.
+            """
+            if mode not in self.modes:
+                raise ValueError(f"Unknown dialog mode {mode}")
+            method = self.modes[mode]["method"]
+            if title is None:
+                title = self.modes[mode]["caption"]
+            if patterns is None:
+                patterns = {"*.*": "All Files", "*.py": "Python Files"}
+            patterns = ";;".join([f"{v} ({k})" for k, v in patterns.items()])
+            options = QFileDialog.Options()
 
-        ret = method(self.dialog, **kwargs)
+            kwargs = {"caption": title, "directory": start, "filter": patterns, "options": options}
+            kwargs = {k: kwargs[k] for k in (set(kwargs.keys()) & set(self.modes[mode]["arg"]))}
 
-        if isinstance(ret, tuple):
-            ret = ret[0]
-        if isinstance(ret, (str, pathlib.PurePath)):
-            ret = pathlib.Path(ret)
-        elif isinstance(ret, list):
-            ret = [pathlib.Path(x) for x in ret]
-        elif not ret:
-            ret = None
-        else:
-            raise TypeError(f"Something when wrong here - can't handle {ret} as a {type(ret)}")
-        return ret
+            ret = method(self.dialog, **kwargs)
+
+            if isinstance(ret, tuple):
+                ret = ret[0]
+            if isinstance(ret, (str, pathlib.PurePath)):
+                ret = pathlib.Path(ret)
+            elif isinstance(ret, list):
+                ret = [pathlib.Path(x) for x in ret]
+            elif not ret:
+                ret = None
+            else:
+                raise TypeError(f"Something when wrong here - can't handle {ret} as a {type(ret)}")
+            return ret
 
 
 fileDialog: Type[App] = App()
