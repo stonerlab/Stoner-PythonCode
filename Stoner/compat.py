4c60824a2a713e8aac3e0dfa383642a9bc8c7485
Gavin Burnell <g.burnell@leeds.ac.uk>
2021-11-06 17:34:23 +0000

    Add missing dependencies for pyface

--- Stoner/compat.py
+++ Stoner/compat.py
@@ -36,9 +36,6 @@ _lmfit = True
 
 try:
     import hyperspy as hs  # Workaround an issue in hs 1.5.2 conda packages
-    from traits.etsconfig.api import ETSConfig
-
-    ETSConfig.toolkit = "qt"
 
     try:
         load = hs.load
