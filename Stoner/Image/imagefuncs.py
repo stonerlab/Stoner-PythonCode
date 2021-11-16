fd829240b207fa3b597b555f94b659cca5d4f433
Gavin Burnell <g.burnell@leeds.ac.uk>
2021-11-16 10:51:43 +0000

    Fix issue when masks are used in phase correlation

--- Stoner/Image/imagefuncs.py
+++ Stoner/Image/imagefuncs.py
@@ -496,7 +496,7 @@ def correct_drift(im, ref, threshold=0.005, upsample_factor=50, box=False, do_sh
 
     shift = registration.phase_cross_correlation(
         refed, imed, upsample_factor=upsample_factor, reference_mask=~np.isnan(refed), moving_mask=~np.isnan(imed)
-    )[0]
+    )
     if do_shift:
         im = im.translate(translation=(-shift[1], -shift[0]))  # x,y
     im.metadata["correct_drift"] = (-shift[1], -shift[0])
