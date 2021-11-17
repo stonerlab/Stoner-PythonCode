093b15b90e237173a521fcfa0f48737a6e929b1d
Gavin Burnell <g.burnell@leeds.ac.uk>
2021-11-17 18:46:14 +0000

    Replace correct_drift algorithm with scharr alignment

--- Stoner/Image/imagefuncs.py
+++ Stoner/Image/imagefuncs.py
@@ -453,7 +453,7 @@ def convert(image, dtype, force_copy=False, uniform=False, normalise=True):
     return image.astype(dtype)
 
 
-def correct_drift(im, ref, threshold=0.005, upsample_factor=50, box=False, do_shift=True):
+def correct_drift(im, ref, **kargs):
     """Align images to correct for image drift.
 
     Args:
@@ -476,30 +476,15 @@ def correct_drift(im, ref, threshold=0.005, upsample_factor=50, box=False, do_sh
     Adds 'drift_shift' to the metadata as the (x,y) vector that translated the
     image back to it's origin.
     """
-    if isinstance(box, bool) and not box:
-        im = im.clone.crop(box=box)
-    cls = type(im)
+    do_shift = kargs.pop("do_shift", True)
+    kargs["scale"] = kargs.pop("upscale", kargs.get("scale", 5.0))
+    kargs.setdefault("meothd", "scharr")
 
-    refed = cls(ref, get_metadata=False)
-    refed = refed.crop(box=box)
-    refed = refed.filter_image(sigma=1)
-    refed = refed > refed.threshold_otsu()
-    refed = refed.corner_fast(threshold=threshold)
-
-    imed = im.clone
-    imed = imed.filter_image(sigma=1)
-    imed = imed > imed.threshold_otsu()
-    imed = imed.corner_fast(threshold=threshold)
-
-    refed = refed.view(np.ndarray)
-    imed = imed.view(np.ndarray)
-
-    shift = registration.phase_cross_correlation(
-        refed, imed, upsample_factor=upsample_factor, reference_mask=~np.isnan(refered), moving_mask=~np.isnan(imed)
-    )[0]
+    ret = align(im, ref, **kargs)
     if do_shift:
-        im = im.translate(translation=(-shift[1], -shift[0]))  # x,y
-    im.metadata["correct_drift"] = (-shift[1], -shift[0])
+        im = ret
+    im["correct_drift"] = -np.array(ret["tvec"])[::-1]
+
     return im
 
 
